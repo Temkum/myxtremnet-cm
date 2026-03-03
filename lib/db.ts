8c0b9476c61218c4fcb70ae05d26d7e82ded2ce8
Kum Jude Tem <judekum14@gmail.com>
2026-03-03 13:08:44 +0100

    bug: adjust db settings for connection - increase request timeout option - update proxy for clean urls

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,9 @@
-import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from '@/db/schema/auth-schema';
 
 export const pgPool = new Pool({
   connectionString: process.env.DATABASE_URL!,
-  ssl: { rejectUnauthorized: false },
 });
 
 export const db = drizzle(pgPool, { schema });
