dd532a6c260b7618f612cb450fda515f9d6ca867
Kum Jude Tem <judekum14@gmail.com>
2026-03-09 12:09:47 +0100

    update db config and run migrations to fix login bug

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,13 @@
-import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from '@/db/schema/auth-schema';
 
 export const pgPool = new Pool({
   connectionString: process.env.DATABASE_URL!,
-  ssl: { rejectUnauthorized: false },
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 export const db = drizzle(pgPool, { schema });
