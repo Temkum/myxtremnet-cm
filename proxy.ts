718cc44fbf779e358b2afd87b7a565a260bb06d6
Kum Jude Tem <judekum14@gmail.com>
2026-03-13 02:20:46 +0100

    adjust pattern for static files

--- proxy.ts
+++ proxy.ts
@@ -47,5 +47,5 @@ export default async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/|.*\\..*).*)'],
 };
